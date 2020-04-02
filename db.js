539c38886c0e437bc4892af659f94cc321399609
tom-rose-codeman <tomrlrose@gmail.com>
2020-04-03 12:20:21 +1300

    add homepage code

---
+++ db.js
@@ -0,0 +1,17 @@
+const knex = require('knex')
+const config = require('./knexfile')
+const env = 'development'
+const database = knex(config[env])
+
+function addData(newData, db = database) {
+    console.log('here is' + newData.name)
+    return db('users').insert({name: newData.name, pic_link: newData.pic_link})
+}
+function showData(db=database) {
+    return db('users').select()
+}
+
+module.exports = {
+    addData,
+    showData,
+}
--- server/server.js
+++ server/server.js
@@ -2,16 +2,20 @@ const express = require('express')
 const hbs = require('express-handlebars')
 
 const server = express()
-
+const landingPageRoutes = require('../routes/landingPage.js')
 // Middleware
 
 server.engine('hbs', hbs({extname: 'hbs'}))
 server.set('view engine', 'hbs')
 server.use(express.urlencoded({extended: true}))
 server.use(express.static('public'))
+server.use('/', landingPageRoutes)
 
 // Routes
-//test
+server.use('/', landingPageRoutes)
+
+    
+
 
 
 module.exports = server;
